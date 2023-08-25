2e3d99330bbfdd50382250875aad79e5b24aeaa3
Gowtham Ullangula <gowthammdb7@gmail.com>
2023-08-25 19:28:23 +0530

    logout commit

--- src/context.js
+++ src/context.js
@@ -7,15 +7,15 @@ export function AuthContextProvider({children}){
     const [tknData,setTokenData] = useState(()=>{
         let data = {tkn:'',author:''}
         let token = localStorage.getItem('tkn')
-        data.tkn = token !== '' ? token : ''
+        data.tkn = token !== null ? token : ''
         let author = localStorage.getItem('author')
-        data.author = author !== '' ? author : ''
+        data.author = author !== null? author : ''
         return data
     })
 
     useEffect(()=>{
         localStorage.setItem('tkn',tknData.tkn)
-        localStorage.setItem('author',tknData.author)   
+        localStorage.setItem('author',tknData.author)
     },[tknData])
 
     return <authContext.Provider value={{tknData,triggerSetToken:(tkn,author)=>setTokenData({tkn,author})}}>
--- src/logout/logout.js
+++ src/logout/logout.js
@@ -21,7 +21,7 @@ function Logout(){
 
     async function handleLogout(){
         if(tknData.tkn !== ''){
-            var res = await fetch('https://feedpedia.onrender.com/logout',{
+            var res = await fetch('http://localhost:5000/logout',{
                 method:'DELETE',
                 headers:{authorization:`Bearer ${tknData.tkn}`}
             })
